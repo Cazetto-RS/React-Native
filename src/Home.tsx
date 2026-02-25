579e32feef01933e018b018dfa783eced0e366a1
Cazetto-RS <rafael.sdct@hotmail.com>
2026-02-25 17:36:56 -0300

    Primeiro commit - Base do projeto | 02/25

---
+++ src/Home.tsx
@@ -0,0 +1,13 @@
+import {View, Text} from "react-native"
+import StyleHome from "./StyleHome"
+import styles from "./StyleHome"
+
+export default function(){
+    return (
+        <View style={styles.container}>
+            <Text>
+                Fala Fiote!
+            </Text>
+        </View>
+    );
+} 
